859b9fbdc518d9f90f7b45f7193781318a949fcd
ShivramR <49326544+ShivramR@users.noreply.github.com>
2019-09-29 23:34:13 -0400

    Fall Fest Initial 2

--- src/main/java/frc/robot/commands/other/TrackObject.java
+++ src/main/java/frc/robot/commands/other/TrackObject.java
@@ -10,18 +10,18 @@ import frc.robot.RobotMap;
 
 public class TrackObject extends Command {
 
-    private NetworkTableEntry blockX;
-    private NetworkTableEntry blockY;
-    private double oldX;
-    private double oldY;
+	private NetworkTableEntry blockX;
+	private NetworkTableEntry blockY;
+	private double oldX;
+	private double oldY;
 
-    public TrackObject() {
+	public TrackObject() {
 		super("TrackObject");
 		requires(Subsystems.driveBase);
-    }
+	}
 
-    @Override
-    public void initialize() {
+	@Override
+	public void initialize() {
 		NetworkTableInstance inst = NetworkTableInstance.getDefault();
 		NetworkTable pixy = inst.getTable("pixy");
 		blockX = pixy.getEntry("blockX");
@@ -29,10 +29,10 @@ public class TrackObject extends Command {
 		Subsystems.driveBase.zeroEncoderPosition();
 		Subsystems.driveBase.zeroGyroAngle();
 		//this.setInterruptible(true);
-    }
+	}
 
-    @Override
-    public void execute() {
+	@Override
+	public void execute() {
 		double correction = (160.0d - blockX.getDouble(-404)) / 160.0d;
 		//correction *= 0.17d;
 		//correction += 1d;
@@ -43,38 +43,38 @@ public class TrackObject extends Command {
 		} 
 		else {
 			if (Subsystems.cargo.getIntakeBeamBroken()) {
-			Subsystems.cargo.stopIntakeMotors();
-			Subsystems.driveBase.setMotors(0, 0);
+				Subsystems.cargo.stopIntakeMotors();
+				Subsystems.driveBase.setMotors(0, 0);
 			} else {
-			Subsystems.driveBase.setMotors(0.25, 0.25);
+				Subsystems.driveBase.setMotors(0.25, 0.25);
 			}
 			
 			if (Subsystems.cargo.getEscalatorBeamBroken()) {
-			Subsystems.cargo.stopIntakeMotors();
-			Subsystems.cargo.stopEscalatorMotors();
+				Subsystems.cargo.stopIntakeMotors();
+				Subsystems.cargo.stopEscalatorMotors();
 			} else {
-			Subsystems.cargo.setIntakeMotors(-0.75);
-			Subsystems.cargo.setEscalatorMotors(-0.75);
+				Subsystems.cargo.setIntakeMotors(-0.75);
+				Subsystems.cargo.setEscalatorMotors(-0.75);
 			}
 		}
-    }
+	}
 
-    @Override
-    public boolean isFinished() {
+	@Override
+	public boolean isFinished() {
 		return Subsystems.cargo.getEscalatorBeamBroken();
-    }
+	}
 
-    @Override
-    public void interrupted() {
+	@Override
+	public void interrupted() {
 		Subsystems.driveBase.setMotors(0, 0);
 		Subsystems.cargo.setEscalatorMotors(0);
 		Subsystems.cargo.setIntakeMotors(0);    
-    }
+	}
 
-    @Override
-    public void end() {
+	@Override
+	public void end() {
 		Subsystems.driveBase.setMotors(0, 0);
 		Subsystems.cargo.setEscalatorMotors(0);
 		Subsystems.cargo.setIntakeMotors(0);
-    } 
+	} 
 } 
